aa582dc56032b22f6129ba644b54e6aa017ef6bb
Amy Vaillancourt-Sals & Carolyn Huynh <student@epicodus.com>
2014-08-14 14:08:15 -0700

    initial commit

---
+++ db/migrate/20140814205927_descriptive_migration_name.rb
@@ -0,0 +1,11 @@
+class DescriptiveMigrationName < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.column :name, :string
+      t.column :done, :boolean
+
+      t.timestamps
+    end
+  end
+end
+
